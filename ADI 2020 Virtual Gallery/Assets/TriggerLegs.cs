d6370cac59bb548b8cf947417cd698b9b1b29bc7
algebraicAdvent3 <tamamaroro@gmail.com>
2020-04-25 17:44:55 -0400

    Added some elements to John's area (may need new materials)

---
+++ ADI 2020 Virtual Gallery/Assets/TriggerLegs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLegs : MonoBehaviour
+{
+    public GameObject legs;
+    public float moveDistance = 10f;
+    public float animationTime = 1f;
+    bool legsVisible = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (legsVisible)
+        {
+            iTween.MoveBy(legs, iTween.Hash("x", -5, "z", moveDistance, "time", animationTime, "easeType", "easeInOutExpo"));
+        }
+        else
+        {
+            iTween.MoveBy(legs, iTween.Hash("x", 5, "z", -moveDistance, "time", animationTime, "easeType", "easeInOutExpo"));
+        }
+        legsVisible = !legsVisible;
+    }
+}
