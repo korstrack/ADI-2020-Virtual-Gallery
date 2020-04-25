36686ee729f0010270c8d6c767defd7ad8d143be
algebraicAdvent3 <tamamaroro@gmail.com>
2020-04-25 16:09:40 -0400

    Added trail renderer for John's area, videoplayer test

---
+++ ADI 2020 Virtual Gallery/Assets/scripts/AnimatedTexture.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        var audioSource = gameObject.AddComponent<AudioSource>();
+
+        videoPlayer.url = "TerrorizeTheBoy.mp4";
+        videoPlayer.playOnAwake = false;
+       //videoPlayer.clip = videoClip;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        videoPlayer.targetMaterialProperty = "_MainTex";
+        videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            var vp = GetComponent<UnityEngine.Video.VideoPlayer>();
+
+            if (vp.isPlaying)
+            {
+                vp.Pause();
+            }
+            else
+            {
+                vp.Play();
+            }
+        }
+    }
+}
---
+++ ADI 2020 Virtual Gallery/Assets/scripts/TriggerTrailRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTrailRenderer : MonoBehaviour
+{
+    public GameObject trailRenderer;
+    public GameObject jetpackTrailL;
+    public GameObject jetpackTrailR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        trailRenderer.SetActive(!trailRenderer.activeSelf);
+        jetpackTrailL.SetActive(!jetpackTrailL.activeSelf);
+        jetpackTrailR.SetActive(!jetpackTrailR.activeSelf);
+    }
+}
