347d9f446331f352f158ba483d084947b0a947e1
korstrack <korstrack.tereans@gmail.com>
2020-04-25 23:42:05 -0400

    added quieter footsteps in the forest

--- ADI 2020 Virtual Gallery/Assets/TrailToggle.cs
+++ ADI 2020 Virtual Gallery/Assets/TrailToggle.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class TrailToggle : MonoBehaviour
 {
 	public GameObject Trail = null;
+	// private Component Footsteps = null;
+	// void start()
+	// {
+	// 	Footsteps = this.GetComponent<AudioSource>();
+	// }
+
+
     // Update is called once per frame
     void Update()
     {
-        if(this.transform.position.z < -100 && this.transform.position.x > -25 && this.transform.position.x < 75){
-        	Trail.SetActive(true);
+      //   if(this.transform.position.z < -100f && this.transform.position.x > -25f && this.transform.position.x < 75f){
+      //   	Trail.SetActive(true);
+      //   }
+      //   else{
+      //   	if(Trail.activeSelf){
+	     //    	Trail.SetActive(false);
+    		// }
+      //   }
+
+        if(this.transform.position.x < -165){
+        	//Footsteps.volume = 0.5;
+        	this.GetComponent<AudioSource>().volume = 0.25f;
         }
         else{
-        	if(Trail.activeSelf){
-	        	Trail.SetActive(false);
-    		}
+        	this.GetComponent<AudioSource>().volume = 1f;
         }
     }
 }
