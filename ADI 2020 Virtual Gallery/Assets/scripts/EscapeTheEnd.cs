3c65c49d5616e93f83efa5760a7f93338fd50a92
korstrack <korstrack.tereans@gmail.com>
2020-04-29 17:27:06 -0400

    signs, pumpkins, dead art

--- ADI 2020 Virtual Gallery/Assets/scripts/EscapeTheEnd.cs
+++ ADI 2020 Virtual Gallery/Assets/scripts/EscapeTheEnd.cs
@@ -7,6 +7,7 @@ public class EscapeTheEnd : MonoBehaviour
 
 	public GameObject getOut = null;
 	public GameObject Controls = null;
+	public GameObject EscHowto = null;
 
     // Update is called once per frame
     void Update()
@@ -16,12 +17,14 @@ public class EscapeTheEnd : MonoBehaviour
         	if(getOut.activeSelf){
         		getOut.SetActive(false);
         		Controls.SetActive(false);
+        		EscHowto.SetActive(false);
         		Cursor.lockState = CursorLockMode.Locked;
         		Cursor.visible = false;
         	}
         	else{
 	        	getOut.SetActive(true);
 	        	Controls.SetActive(true);
+	        	EscHowto.SetActive(true);
 	        	Cursor.lockState = CursorLockMode.None;
 	        	Cursor.visible = true;  	
 	        }
